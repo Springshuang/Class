21b38ce30498647809c1b56c496fc3a79345bb43
Springshuang <997535329@qq.com>
2018-06-22 16:19:35 +0800

    add

---
+++ helloclass.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include"helloclass.h"
+using namespace std;
+
+HelloClass::HelloClass()
+{
+    cout<<"I'm being created."<<endl;
+}
+
+HelloClass::~HelloClass()
+{
+    cout<<"I'm being destroyed."<<endl;
+}
+
+void HelloClass::sayHello()
+{
+    cout<<"Hello Class!"<<endl;
+}
